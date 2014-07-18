6de97ea553c5085d12ae09d191eb12abd91065cc
Luke McComb <lucasmccomb@gmail.com>
2014-07-18 02:37:35 -0600

    app prints title, keywords, artist name, copyright, create date, and lens model

--- exif_viewer.rb
+++ exif_viewer.rb
@@ -1,8 +1,21 @@
 require 'mini_exiftool'
 
-photo = MiniExiftool.new "dog.jpg"
+photo = MiniExiftool.new "horses.jpg"
 
-puts photo[:ImageDescription]
+puts "˚ "*10
+puts "Image Title:\n #{photo[:Title]}"
+puts "˚ "*10
+puts "Image Keywords:\n #{photo[:ImageDescription]}"
+puts "˚ "*10
+puts "Artist:\n #{photo[:Artist]}"
+puts "˚ "*10
+puts "Copyright:\n #{photo[:Copyright]}"
+puts "˚ "*10
+puts "Create Date:\n #{photo[:CreateDate]}"
+puts "˚ "*10
+puts "Lens Model:\n #{photo[:LensModel]}"
+puts "˚ "*10
+
 
 # open /Users/LM/Pictures/20140717/20090926©LEM-281.jpg
 # ruby exif_viewer.rb
